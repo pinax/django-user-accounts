029ea6e0733ffb1bf30f9281e8d8979f93a7fd47
Brian Rosner <brosner@gmail.com>
2012-03-10 13:49:22 -0800

    initial commit of README and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name = "django-user-accounts",
+    version = "0.9b1.dev1",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "a Django user account app",
+    long_description = open("README.rst").read(),
+    license = "MIT",
+    url = "http://github.com/pinax/django-user-accounts",
+    packages = [
+        "account",
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
