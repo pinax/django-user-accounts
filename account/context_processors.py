7d5de6a7235bd2fe7616a92068db49ab15db7afc
Brian Rosner <brosner@gmail.com>
2012-03-12 14:15:57 -0700

    added ACCOUNT_CONTACT_EMAIL

--- account/context_processors.py
+++ account/context_processors.py
@@ -4,5 +4,6 @@ from account.conf import settings
 def account(request):
     ctx = {
         "ACCOUNT_OPEN_SIGNUP": settings.ACCOUNT_OPEN_SIGNUP,
+        "ACCOUNT_CONTACT_EMAIL": settings.ACCOUNT_CONTACT_EMAIL,
     }
     return ctx
