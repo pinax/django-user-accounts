999a7c743f079d6d7e2a0b870790b7294fae08d2
Brian Rosner <brosner@gmail.com>
2012-04-21 14:40:14 -0600

    updated SignupForm to match password field convention naming

--- account/forms.py
+++ account/forms.py
@@ -21,11 +21,11 @@ class SignupForm(forms.Form):
         widget=forms.TextInput(),
         required=True
     )
-    password1 = forms.CharField(
+    password = forms.CharField(
         label=_("Password"),
         widget=forms.PasswordInput(render_value=False)
     )
-    password2 = forms.CharField(
+    password_confirm = forms.CharField(
         label=_("Password (again)"),
         widget=forms.PasswordInput(render_value=False)
     )
@@ -52,8 +52,8 @@ class SignupForm(forms.Form):
         raise forms.ValidationError(_("A user is registered with this email address."))
     
     def clean(self):
-        if "password1" in self.cleaned_data and "password2" in self.cleaned_data:
-            if self.cleaned_data["password1"] != self.cleaned_data["password2"]:
+        if "password" in self.cleaned_data and "password_confirm" in self.cleaned_data:
+            if self.cleaned_data["password"] != self.cleaned_data["password_confirm"]:
                 raise forms.ValidationError(_("You must type the same password each time."))
         return self.cleaned_data
 
