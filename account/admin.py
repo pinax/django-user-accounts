3676d2c7ea0e9a5c3f8510ac8e6f8d58175e6b5f
Brian Rosner <brosner@gmail.com>
2012-05-08 21:30:41 -0600

    added admin.py with SignupCode

---
+++ account/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from account.models import SignupCode
+
+
+class SignupCodeAdmin(admin.ModelAdmin):
+    list_display = ["code", "max_uses", "use_count", "expiry", "created"]
+    search_fields = ["code", "email"]
+    list_filter = ["created"]
+
+
+admin.site.register(SignupCode, SignupCodeAdmin)
