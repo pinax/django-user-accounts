4181949ac1825ad9eabe92c1c9110f32f65d5e9a
Brian Rosner <brosner@gmail.com>
2012-06-07 14:20:25 -0600

    Added missing receiver import

--- account/models.py
+++ account/models.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.utils import timezone, translation
 from django.utils.translation import gettext_lazy as _
