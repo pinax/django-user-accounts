20e6aa044b705a647968af8f1ae79381a7bf113c
Brian Rosner <brosner@gmail.com>
2012-08-21 15:52:45 -0600

    Added Account model to admin.py
    Account will now show up in the Django admin interface.
    
    Fixes #28 — thanks to Douglas Meehan for the issue report.

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
-from account.models import SignupCode, AccountDeletion
+from account.models import Account, SignupCode, AccountDeletion
 
 
 class SignupCodeAdmin(admin.ModelAdmin):
+    
     list_display = ["code", "max_uses", "use_count", "expiry", "created"]
     search_fields = ["code", "email"]
     list_filter = ["created"]
 
 
+admin.site.register(Account)
 admin.site.register(SignupCode, SignupCodeAdmin)
 admin.site.register(AccountDeletion, list_display=["email", "date_requested", "date_expunged"])
