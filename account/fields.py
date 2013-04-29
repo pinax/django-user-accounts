b129ec6e02258fc1d0eb6e3966b488db0ed521de
George Marshall <echosx@gmail.com>
2013-04-29 04:26:48 -0700

    Added support for south

--- account/fields.py
+++ account/fields.py
@@ -2,11 +2,17 @@ from django.db import models
 
 from account.conf import settings
 
+HAS_SOUTH = True
+try:
+    from south.modelsinspector import add_introspection_rules
+except ImportError:
+    HAS_SOUTH = False
+
 
 class TimeZoneField(models.CharField):
-    
+
     __metaclass__ = models.SubfieldBase
-    
+
     def __init__(self, *args, **kwargs):
         defaults = {
             "max_length": 100,
@@ -16,3 +22,7 @@ class TimeZoneField(models.CharField):
         }
         defaults.update(kwargs)
         return super(TimeZoneField, self).__init__(*args, **defaults)
+
+
+if HAS_SOUTH:
+    add_introspection_rules([], ["^account\.fields\.TimeZoneField"])
