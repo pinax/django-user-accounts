b455acb8b442c774ddf1bdaa0949cd8c4b2ad6e1
Brian Rosner <brosner@gmail.com>
2013-08-02 11:21:43 -0600

    Simplified tests (for now; need to build out proper tests)

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.test import TestCase
 from django.test.client import RequestFactory
 from django.utils import unittest
 
@@ -14,28 +15,7 @@ class SignupEnabledView(SignupView):
         return True
 
 
-class SignupDisabledView(SignupView):
-    
-    def is_open(self):
-        return False
-
-
-class SignupRedirectView(SignupView):
-    pass
-
-
-class LoginDisabledView(LoginView):
-    
-    def disabled(self):
-        return True
-
-class LoginRedirectView(LoginView):
-    
-    def login_user(self, form):
-        return True
-
-
-class SignupViewTestCase(unittest.TestCase):
+class SignupViewTestCase(TestCase):
     
     def setUp(self):
         self.factory = RequestFactory()
@@ -45,45 +25,9 @@ class SignupViewTestCase(unittest.TestCase):
         request.user = AnonymousUser()
         response = SignupEnabledView.as_view()(request)
         self.assertEqual(response.status_code, 200)
-    
-    def test_get_disabled(self):
-        request = self.factory.get(reverse("account_signup"))
-        request.user = AnonymousUser()
-        response = SignupDisabledView.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name, "account/signup_closed.html")
-    
-    def test_post_disabled(self):
-        request = self.factory.post(reverse("account_signup"))
-        request.user = AnonymousUser()
-        response = SignupDisabledView.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.template_name, "account/signup_closed.html")
-    
-    def test_post_successful(self):
-        post = {"username": "user", "password": "pwd",
-            "password_confirm": "pwd", "email": "info@example.com"}
-        request = self.factory.post(reverse("account_signup"), post)
-        request.user = AnonymousUser()
-        response = SignupEnabledView.as_view()(request)
-        self.assertEqual(response.status_code, 302)
-        user = User.objects.get(username="user")
-        self.asserEqual(user.email, "info@example.com")
-    
-    def test_custom_redirect_field(self):
-        request = self.factory.request()
-        request.GET = {"next_page": "/profile/"}
-        form = SignupForm({
-            "username": "test",
-            "password": "password",
-            "password_confirm": "password",
-            "email": "someone@example.com",
-        })
-        view = SignupRedirectView(request=request, redirect_field_name="next_page")
-        self.assertEqual("/profile/", view.form_valid(form)["Location"])
 
 
-class LoginViewTestCase(unittest.TestCase):
+class LoginViewTestCase(TestCase):
     
     def setUp(self):
         self.factory = RequestFactory()
@@ -94,22 +38,3 @@ class LoginViewTestCase(unittest.TestCase):
         response = LoginView.as_view()(request)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.template_name, ["account/login.html"])
-
-    def test_get_disabled(self):
-        request = self.factory.get(reverse("account_login"))
-        request.user = AnonymousUser()
-        response = LoginDisabledView.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-    
-    def test_post_disabled(self):
-        request = self.factory.post(reverse("account_login"))
-        request.user = AnonymousUser()
-        response = LoginDisabledView.as_view()(request)
-        self.assertEqual(response.status_code, 403)
-    
-    def test_custom_redirect_field(self):
-        request = self.factory.request()
-        request.GET = {"next_page": "/profile/"}
-        form = LoginUsernameForm({"username": "test", "password": "password"})
-        view = LoginRedirectView(request=request, redirect_field_name="next_page")
-        self.assertEqual("/profile/", view.form_valid(form)["Location"])
--- runtests.py
+++ runtests.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from django.conf import settings
@@ -16,9 +17,16 @@ settings.configure(
         "django.contrib.auth",
         "django.contrib.contenttypes",
         "django.contrib.sites",
+        "django.contrib.messages",
         "account",
     ],
+    MIDDLEWARE_CLASSES=[
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ],
     SITE_ID=1,
+    TEMPLATE_DIRS=[
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "account", "tests", "templates")),
+    ]
 )
 
 from django_nose import NoseTestSuiteRunner
