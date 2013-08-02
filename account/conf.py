38d8aa5d180ed5b18fc6901bc237d369153a0ba0
Brian Rosner <brosner@gmail.com>
2013-08-02 08:32:30 -0600

    Updated except syntax

--- account/conf.py
+++ account/conf.py
@@ -13,7 +13,7 @@ def load_path_attr(path):
     module, attr = path[:i], path[i+1:]
     try:
         mod = importlib.import_module(module)
-    except ImportError, e:
+    except ImportError as e:
         raise ImproperlyConfigured("Error importing %s: '%s'" % (module, e))
     try:
         attr = getattr(mod, attr)
