ec3325ad9441983d8f9a50e1967e5b4c1b3c3c70
Brian Rosner <brosner@gmail.com>
2013-08-02 10:02:39 -0600

    Added Travis and runtests.py

---
+++ runtests.py
@@ -0,0 +1,30 @@
+import sys
+
+from django.conf import settings
+
+
+settings.configure(
+    DEBUG=True,
+    USE_TZ=True,
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+        }
+    },
+    ROOT_URLCONF="account.urls",
+    INSTALLED_APPS=[
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sites",
+        "account",
+    ],
+    SITE_ID=1,
+)
+
+from django_nose import NoseTestSuiteRunner
+
+test_runner = NoseTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(["account"])
+
+if failures:
+    sys.exit(failures)
