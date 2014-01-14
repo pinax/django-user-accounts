0007ea4aa0f7ebadfadb0c6f605c51a1d11e483c
Brian Rosner <brosner@gmail.com>
2014-01-14 09:12:02 -0700

    Allow IntegrityError to propagate with duplicate email
    Fixes #62. When ACCOUNT_EMAIL_UNIQUE is True we should fail loudly when an
    attempt to insert a duplicate occurs. Let the callers handle the failure.

--- account/managers.py
+++ account/managers.py
@@ -7,14 +7,10 @@ class EmailAddressManager(models.Manager):
 
     def add_email(self, user, email, **kwargs):
         confirm = kwargs.pop("confirm", False)
-        try:
-            email_address = self.create(user=user, email=email, **kwargs)
-        except IntegrityError:
-            return None
-        else:
-            if confirm and not email_address.verified:
-                email_address.send_confirmation()
-            return email_address
+        email_address = self.create(user=user, email=email, **kwargs)
+        if confirm and not email_address.verified:
+            email_address.send_confirmation()
+        return email_address
 
     def get_primary(self, user):
         try:
