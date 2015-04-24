e967202361462c83be981b1d3172a3c2b1d5228c
Brian Rosner <brosner@gmail.com>
2015-04-24 09:37:44 -0600

    Test closed sign up

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -18,6 +18,18 @@ class SignupViewTestCase(TestCase):
         response = self.client.post(reverse("account_signup"), data)
         self.assertEqual(response.status_code, 302)
 
+    def test_closed(self):
+        with self.settings(ACCOUNT_OPEN_SIGNUP=False):
+            data = {
+                "username": "foo",
+                "password": "bar",
+                "password_confirm": "bar",
+                "email": "foobar@example.com",
+            }
+            response = self.client.post(reverse("account_signup"), data)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.template_name, "account/signup_closed.html")
+
 
 class LoginViewTestCase(TestCase):
 
