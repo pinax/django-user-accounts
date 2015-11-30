4b64fd38241c5fb550bf6edf3138c34847e01ff8
Patrick Altman <paltman@gmail.com>
2015-11-30 11:40:17 -0600

    Upgrade to support Django master (1.10)

--- account/fields.py
+++ account/fields.py
@@ -1,12 +1,20 @@
 from __future__ import unicode_literals
 
+import django
+
 from django.db import models
 from django.utils import six
 
 from account.conf import settings
 
 
-class TimeZoneField(six.with_metaclass(models.SubfieldBase, models.CharField)):
+if django.VERSION < (1, 8):
+    base = six.with_metaclass(models.SubfieldBase, models.CharField)
+else:
+    base = models.CharField
+
+
+class TimeZoneField(base):
 
     def __init__(self, *args, **kwargs):
         defaults = {
