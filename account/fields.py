48609b17bddb465b47aafea25ef5e6760effd63f
Brian Rosner <brian@brosner.com>
2015-12-02 08:13:05 -0700

    Fixed TimeZoneField to subclass CharField directly

--- account/fields.py
+++ account/fields.py
@@ -1,20 +1,11 @@
 from __future__ import unicode_literals
 
-import django
-
 from django.db import models
-from django.utils import six
 
 from account.conf import settings
 
 
-if django.VERSION < (1, 8):
-    base = six.with_metaclass(models.SubfieldBase, models.CharField)
-else:
-    base = models.CharField
-
-
-class TimeZoneField(base):
+class TimeZoneField(models.CharField):
 
     def __init__(self, *args, **kwargs):
         defaults = {
