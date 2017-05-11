6ba486385ee1917047512a274435150a6c5bc49e
Cezary Statkiewicz <cezio@thelirium.net>
2017-05-11 17:34:08 +0200

    Django 2.0 compatibility #252

--- account/tests/urls.py
+++ account/tests/urls.py
@@ -1,9 +1,17 @@
+import django
 from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
+# D 2.0 compatibility
+if django.VERSION[0] < 2:
+    admin_urls = url(r"admin/", include(admin.site.urls))
+else:
+    admin_urls = url(r"admin/", include(admin.site.urls))
+
+
 urlpatterns = [
-    url(r"admin/", include(admin.site.urls)),
+    admin_urls,
     url(r"^", include("account.urls")),
 ]
