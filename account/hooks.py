74692abaa928e20c55c21ac7ba73461b03f5988f
jr-hypertext <jhovel.r@hypertext.com.kw>
2017-07-07 23:19:33 +0300

    added html_message to send_mail function. TO MODIFY EASILY HTML EMAIL TEMPLATE ONCE THE APP IS INSTALLED INSTEAD OF MODIFYING THE HOOKS.

--- account/hooks.py
+++ account/hooks.py
@@ -12,25 +12,29 @@ class AccountDefaultHookSet(object):
     def send_invitation_email(self, to, ctx):
         subject = render_to_string("account/email/invite_user_subject.txt", ctx)
         message = render_to_string("account/email/invite_user.txt", ctx)
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
+        html_message = render_to_string("account/html_email/invite_user.html", ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to, html_message=html_message)
 
     def send_confirmation_email(self, to, ctx):
         subject = render_to_string("account/email/email_confirmation_subject.txt", ctx)
         subject = "".join(subject.splitlines())  # remove superfluous line breaks
         message = render_to_string("account/email/email_confirmation_message.txt", ctx)
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
+        html_message = render_to_string("account/html_email/email_confirmation_message.html", ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to, html_message=html_message)
 
     def send_password_change_email(self, to, ctx):
         subject = render_to_string("account/email/password_change_subject.txt", ctx)
         subject = "".join(subject.splitlines())
         message = render_to_string("account/email/password_change.txt", ctx)
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
+        html_message = render_to_string("account/html_email/password_change.html", ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to, html_message=html_message)
 
     def send_password_reset_email(self, to, ctx):
         subject = render_to_string("account/email/password_reset_subject.txt", ctx)
         subject = "".join(subject.splitlines())
         message = render_to_string("account/email/password_reset.txt", ctx)
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to)
+        html_message = render_to_string("account/html_email/password_reset.html", ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, to, html_message=html_message)
 
     def generate_random_token(self, extra=None, hash_func=hashlib.sha256):
         if extra is None:
