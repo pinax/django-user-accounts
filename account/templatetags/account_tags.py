ea3694235bbc5e56f3f28977eb0d405f1c071c73
Patrick Altman <paltman@gmail.com>
2017-10-07 14:34:42 -0500

    Fix single-quote violation

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -95,7 +95,7 @@ def urlnext(parser, token):
     kwargs = {}
     asvar = None
     bits = bits[2:]
-    if len(bits) >= 2 and bits[-2] == 'as':
+    if len(bits) >= 2 and bits[-2] == "as":
         asvar = bits[-1]
         bits = bits[:-2]
 
