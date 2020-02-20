a6a4da9952d470f23a5a4c88c232797e7592ac86
Patrick Altman <paltman@gmail.com>
2020-02-20 16:21:45 -0600

    3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-user-accounts",
-    version="2.1.0",
+    version="3.0.0",
     author="Brian Rosner",
     author_email="brosner@gmail.com",
     description="a Django user account app",
