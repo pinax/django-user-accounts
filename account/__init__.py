46dc6e167acf0d27b100615c3d1d3a35b38febf4
blag <blag@users.noreply.github.com>
2023-09-07 01:10:09 -0600

    Support specifying the version in pyproject.toml, and support Python 3.11+ at the same time

--- account/__init__.py
+++ account/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "3.3.0"
+try:
+	from importlib.metadata import version
+except ImportError:
+	import pkg_resources
+	__version__ = pkg_resources.get_distribution("django-user-accounts").version
+else:
+	__version__ = version("django-user-accounts")
